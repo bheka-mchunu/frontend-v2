8b434842d3c8d24e961906ef3f8ae7583239c663
unknown <marcosftp1@gmail.com>
2021-08-27 09:51:41 -0300

    added button earn

--- src/components/navs/AppNav/AppNavToggle.vue
+++ src/components/navs/AppNav/AppNavToggle.vue
@@ -4,11 +4,18 @@
       :to="{ name: 'home' }"
       :class="[
         'toggle-link pl-6 pr-4 border-l rounded-l-full',
-        { [activeClasses]: !isInvestPage && !isPoolPage }
+        { [activeClasses]: !isInvestPage && !isPoolPage && !isEarnPage }
       ]"
       @click="trackGoal(Goals.ClickNavTrade)"
     >
       {{ $t('trade') }}
+    </router-link>
+    <router-link
+      :to="{ name: 'earn' }"
+      :class="['toggle-link pl-4 pr-6', { [activeClasses]: isEarnPage }]"
+      @click="trackGoal(Goals.ClickNavInvest)"
+    >
+      {{ $t('Earn') }}
     </router-link>
     <router-link
       :to="{ name: 'invest' }"
@@ -36,12 +43,14 @@ export default defineComponent({
     const activeClasses = 'bg-black border-black text-white';
     const isInvestPage = computed(() => route.name === 'invest');
     const isPoolPage = computed(() => route.name === 'pool');
+    const isEarnPage = computed(() => route.name === 'earn');
     const { trackGoal, Goals } = useFathom();
 
     return {
       isInvestPage,
       isPoolPage,
       activeClasses,
+      isEarnPage,
       trackGoal,
       Goals
     };
