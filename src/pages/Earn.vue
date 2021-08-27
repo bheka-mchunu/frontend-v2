a9687d9541933520c52306edb70a43dd75eaa6c5
unknown <marcosftp1@gmail.com>
2021-08-27 09:54:24 -0300

    Earn page

--- src/pages/Earn.vue
+++ src/pages/Earn.vue
@@ -1,13 +1,11 @@
 <template>
-    <div class="flex justify-center flex-col m-4">
-     
-    </div>
+  <div class="flex justify-center flex-col m-4"></div>
 </template>
 
 <script>
-    import { defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 
-    export default defineComponent({
-        name: 'Earn'
-    });
+export default defineComponent({
+  name: 'Earn'
+});
 </script>
